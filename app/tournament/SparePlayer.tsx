f8191844d9cef7a28060f08eed32fd31cdeffa42
Jeff Horton <jeff@goblinoid.co.uk>
2025-10-27 14:46:26 +0000

    Update to 16 players, add TO list

--- app/tournament/SparePlayer.tsx
+++ app/tournament/SparePlayer.tsx
@@ -1,8 +1,16 @@
 import { AiOutlineFilePdf } from "react-icons/ai";
-import type { Tournament } from "~/tournament/tournament-model.server";
+import type {
+  OrganiserPlayer,
+  Tournament,
+} from "~/tournament/tournament-model.server";
 
 export type SparePlayerProps = {
   tournament: Tournament;
+};
+
+type OrganiserPlayerListProps = {
+  player: OrganiserPlayer;
+  playerDescription: string;
 };
 
 export const SparePlayer = ({ tournament }: SparePlayerProps) => (
@@ -12,18 +20,47 @@ export const SparePlayer = ({ tournament }: SparePlayerProps) => (
         <p>
           The spare player for this tournament is {tournament.sparePlayer.name}.
         </p>
-        {tournament.sparePlayer.listPdfUrl && (
-          <p>
-            <a
-              href={`${tournament.sparePlayer.listPdfUrl.base}${tournament.sparePlayer.listPdfUrl.name}`}
-              download={tournament.sparePlayer.listPdfUrl.name}
-              className="button primary hollow expanded"
-            >
-              <AiOutlineFilePdf /> Download the spare player's list PDF.
-            </a>
-          </p>
-        )}
+        <OrganiserListDownload
+          player={tournament.sparePlayer}
+          playerDescription={"spare player"}
+        />
       </>
+    )}
+  </>
+);
+
+export const LowAttendeesPlayer = ({ tournament }: SparePlayerProps) => (
+  <>
+    {tournament.lowAttendeesPlayer && (
+      <>
+        <p>
+          If needed to qualify the event for UK master's rankings, the
+          tournament organiser will use this list.
+        </p>
+        <OrganiserListDownload
+          player={tournament.lowAttendeesPlayer}
+          playerDescription={"tournament organiser"}
+        />
+      </>
+    )}
+  </>
+);
+
+const OrganiserListDownload = ({
+  player,
+  playerDescription,
+}: OrganiserPlayerListProps) => (
+  <>
+    {player.listPdfUrl && (
+      <p>
+        <a
+          href={`${player.listPdfUrl.base}${player.listPdfUrl.name}`}
+          download={player.listPdfUrl.name}
+          className="button primary hollow expanded"
+        >
+          <AiOutlineFilePdf /> Download the {playerDescription}'s list PDF.
+        </a>
+      </p>
     )}
   </>
 );
